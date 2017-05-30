b8e5d542a265e1d28e48cd0df6dd2d974afe8f27
johncf <john.ch.fr@gmail.com>
2017-05-30 18:29:11 +0530

    possibility of box

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,21 @@ pub use info_tree::InfoTree;
 const MIN_CHILDREN: usize = 8;
 const MAX_CHILDREN: usize = 16;
 
+//trait Mutable: Deref {
+//    fn make_mut(this: &mut Self) -> &mut Self::Target;
+//}
+//
+//impl<T> Mutable for Box<T> {
+//    fn make_mut(this: &mut Box<T>) -> &mut T {
+//        &mut *this
+//    }
+//}
+//
+// Uncomment the above block, and RC can be assigned the type Box<T>
+// This gives around 20-40% speedup!
+// Note that `Arc::clone` or `Rc::clone` methods are never used directly (`clone` may be indirectly
+// called by `make_mut` if ref-count > 1).
+
 type RC<T> = std::sync::Arc<T>; // replace with std::rc::Rc<T> to get significant speed-up.
 type NVec<T> = ArrayVec<[T; MAX_CHILDREN]>;
 
